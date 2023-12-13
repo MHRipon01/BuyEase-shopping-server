6022bc690dea8afba5e3f5af4c02cf645f0d8193
MHRipon01 <136681772+MHRipon01@users.noreply.github.com>
2023-12-14 00:07:47 +0600

    categorywise sort complete

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors())
-app.use(express.json())
-
-
-
+app.use(cors());
+app.use(express.json());
 
 console.log();
 console.log();
@@ -23,11 +20,8 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
-
-
-
 
 async function run() {
   try {
@@ -35,7 +29,9 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -44,30 +40,59 @@ async function run() {
 
 run().catch(console.dir);
 
+const productCollection = client.db("BuyEase").collection("AllProducts");
+const categoryCollection = client.db("BuyEase").collection("categories");
 
-const productCollection = client.db('BuyEase').collection('AllProducts')
+app.get("/api/v1/products", async (req, res) => {
+  const cursor = productCollection.find();
+  const result = await cursor.toArray();
+  res.send(result);
+});
 
-app.get('/api/v1/services' , async(req,res) => {
-    const cursor = productCollection.find()
-    const result = await cursor.toArray()
-    res.send(result)
-})
+//work from here
+// getting data for "people also watching" which is located in product details page
+// app.get('/api/v1/product/:category' , async(req,res) => {
+//   const category = req.params.category
+//   try {
+//     const cursor = productCollection.find({category: category})
+//     const
+//   }
+// })
 
+app.get("/productDetails/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await productCollection.findOne(query);
+  res.send(result);
+});
 
+app.get("/api/v1/productsCount", async (req, res) => {
+  const count = await productCollection.estimatedDocumentCount();
+  res.send({ count });
+});
 
+//getting all categories for home page
+app.get("/allCategories", async (req, res) => {
+  const category = await categoryCollection.find().toArray();
+  res.send(category);
+});
 
+//getting all data on a specific category
+app.get("/api/v1/categoryProducts/:category", async (req, res) => {
+  const category = req.params.category;
+  console.log(category);
+  const productsWithCategory = await productCollection
+    .find({ category: category })
+    .toArray();
+  res.send(productsWithCategory);
+});
 
+//___________________________________________
 
+app.get("/", (req, res) => {
+  res.send("buyease server is running");
+});
 
-
-
-
-
-
-app.get('/' , (req,res) =>{
-    res.send('buyease server is running')
-})
-
-app.listen(port , () => {
-    console.log(`server is running on port: ${port}`);
-})
+app.listen(port, () => {
+  console.log(`server is running on port: ${port}`);
+});
